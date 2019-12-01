19ae4e1a6955c62c39eacb3e00d220958ac8b014
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2019-12-01 20:45:03 +0300

    ENH: forbid dangerous floating-point operations

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+#include <fenv.h>
 #include "engine/engine.h"
 
 
 int main (int argc, char *argv[])
 {
+  feenableexcept (FE_DIVBYZERO | FE_OVERFLOW | FE_INVALID);
   engine_t engine (argc, argv);
   return engine.exec ();
 }
