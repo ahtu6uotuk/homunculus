0b393f977e4c3472a0ba1f40326580f4030638ef
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-01-21 19:45:12 +0300

    ENH: renderer: add render_text func, set viewport to 800x600 and add text shader

--- engine/renderer/renderer.h
+++ engine/renderer/renderer.h
@@ -5,6 +5,8 @@
 
 class engine_t;
 class gui_system_t;
+class shader_t;
+
 namespace sf
 {
   class RenderWindow;
@@ -16,11 +18,14 @@ class renderer_t
   sf::RenderWindow &m_window;
   gui_system_t &m_gui;
   font_t m_font;
+  unique_ptr<shader_t> m_text_shader;
 public:
   renderer_t (engine_t &engine);
   err_t init ();
   void render ();
+  void render_text (const string &text, glm::vec2 &pos, glm::vec3 &color);
   font_t &get_font () {return m_font;}
+  shader_t *get_text_shader () {return m_text_shader.get ();}
 };
 
 #endif // RENDERER_H
