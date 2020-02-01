90160bfb7db0c73cac212e1e8261796b00bdc83d
alastrim <nick.alastrim@gmail.com>
2020-02-01 21:46:21 +0300

    DEL: engine: remove redundant constructor
    (const char * can be implicitly casted to const std::string &)

--- engine/gui/gui_textline.cpp
+++ engine/gui/gui_textline.cpp
@@ -1,34 +1,25 @@
 #include "gui_textline.h"
 #include "engine/renderer/renderer.h"
 
-
-gui_textline_t::gui_textline_t (renderer_t &renderer,
-                                const unsigned int x,
-                                const unsigned int y,
-                                gui_horizontal_alignment_t h_align,
-                                gui_vertical_alignment_t v_align,
-                                const char *text,
-                                const glm::vec3 &color,
-                                const unsigned int font_size,
-                                const gui_element_t *pivot):
-  gui_element_t (renderer, x, y,
-                 renderer.get_font ().get_text_width (text),
-                 renderer.get_font ().get_font_height (),
-                 h_align, v_align, pivot),
-  m_text (text),
-  m_color (color),
-  m_font_size (font_size)
-{}
-
-gui_textline_t::gui_textline_t (renderer_t &renderer,
-                                const unsigned int x,
-                                const unsigned int y,
-                                gui_horizontal_alignment_t h_align,
-                                gui_vertical_alignment_t v_align,
-                                const string &text, const glm::vec3 &color,
-                                const unsigned int font_size, const gui_element_t *pivot):
-  gui_textline_t (renderer, x, y, h_align, v_align, text.c_str (), color, font_size, pivot)
-{}
+gui_textline_t::gui_textline_t (
+    renderer_t &renderer,
+    const unsigned int x,
+    const unsigned int y,
+    gui_horizontal_alignment_t h_align,
+    gui_vertical_alignment_t v_align,
+    const string &text,
+    const glm::vec3 &color,
+    const unsigned int font_size,
+    const gui_element_t *pivot)
+  : gui_element_t (renderer, x, y,
+                   renderer.get_font ().get_text_width (text),
+                   renderer.get_font ().get_font_height (),
+                   h_align, v_align, pivot),
+    m_text (text),
+    m_color (color),
+    m_font_size (font_size)
+{
+}
 
 void gui_textline_t::set_color (const glm::vec3 &color)
 {
