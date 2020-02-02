6de1e6715625722517e672899f0730aceb97043f
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-02-03 02:01:37 +0300

    ENH: GUI: compute textline size using font class

--- engine/gui/gui_context.cpp
+++ engine/gui/gui_context.cpp
@@ -40,23 +40,23 @@ void gui_context_t::handle_mouse_move_event (const int x, const int y)
 void gui_context_t::add_test_page (renderer_t &renderer)
 {
   add_element (make_unique<gui_textline_t> (renderer,
-                                            10, 10, 300, 100,
+                                            10, 10,
                                             gui_horizontal_alignment_t::LEFT, gui_vertical_alignment_t::UP,
                                             "LU", glm::vec3 (.7f, .15f, .15f), 48));
   add_element (make_unique<gui_textline_t> (renderer,
-                                            10, 10, 300, 100,
+                                            10, 10,
                                             gui_horizontal_alignment_t::LEFT, gui_vertical_alignment_t::DOWN,
                                             "LD", glm::vec3 (.7f, .15f, .15f), 48));
   add_element (make_unique<gui_textline_t> (renderer,
-                                            10, 10, 300, 100,
+                                            10, 10,
                                             gui_horizontal_alignment_t::RIGHT, gui_vertical_alignment_t::DOWN,
                                             "боттом кек", glm::vec3 (.7f, .15f, .15f), 48));
   add_element (make_unique<gui_textline_t> (renderer,
-                                            10, 10, 300, 100,
+                                            10, 10,
                                             gui_horizontal_alignment_t::RIGHT, gui_vertical_alignment_t::UP,
                                             "ТОП КЕК", glm::vec3 (.7f, .15f, .15f), 48));
   add_element (make_unique<gui_textline_t> (renderer,
-                                            10, 10, 300, 100,
+                                            10, 10,
                                             gui_horizontal_alignment_t::CENTER, gui_vertical_alignment_t::CENTER,
                                             "ещё один кек", glm::vec3 (.7f, .15f, .15f), 48));
 }
--- engine/gui/gui_textline.cpp
+++ engine/gui/gui_textline.cpp
@@ -5,15 +5,16 @@
 gui_textline_t::gui_textline_t (renderer_t &renderer,
                                 const unsigned int x,
                                 const unsigned int y,
-                                const unsigned int width,
-                                const unsigned int height,
                                 gui_horizontal_alignment_t h_align,
                                 gui_vertical_alignment_t v_align,
                                 const char *text,
                                 const glm::vec3 &color,
                                 const unsigned int font_size,
                                 const gui_element_t *pivot):
-  gui_element_t (renderer, x, y, width, height, h_align, v_align, pivot),
+  gui_element_t (renderer, x, y,
+                 renderer.get_font ().get_text_width (text),
+                 renderer.get_font ().get_font_height (),
+                 h_align, v_align, pivot),
   m_text (text),
   m_color (color),
   m_font_size (font_size)
@@ -22,13 +23,11 @@ gui_textline_t::gui_textline_t (renderer_t &renderer,
 gui_textline_t::gui_textline_t (renderer_t &renderer,
                                 const unsigned int x,
                                 const unsigned int y,
-                                const unsigned int width,
-                                const unsigned int height,
                                 gui_horizontal_alignment_t h_align,
                                 gui_vertical_alignment_t v_align,
                                 const string &text, const glm::vec3 &color,
                                 const unsigned int font_size, const gui_element_t *pivot):
-  gui_textline_t (renderer, x, y, width, height, h_align, v_align, text.c_str (), color, font_size, pivot)
+  gui_textline_t (renderer, x, y, h_align, v_align, text.c_str (), color, font_size, pivot)
 {}
 
 void gui_textline_t::set_color (const glm::vec3 &color)
