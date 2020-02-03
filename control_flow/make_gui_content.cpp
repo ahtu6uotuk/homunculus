51d8443ec9b19e086d17c3388bdea6b10a415d4e
alastrim <nick.alastrim@gmail.com>
2020-02-03 18:59:15 +0300

    ENH: gui: text positions

--- control_flow/make_gui_content.cpp
+++ control_flow/make_gui_content.cpp
@@ -16,14 +16,15 @@ void make_gui_content (engine_t &engine, world_t &world, thread_info_t &thr_info
   unique_ptr<gui_context_t> local_result = make_unique<gui_context_t> (engine.get_gui_system ());
   vector<object_base *> objs = world.get_all ();
 
+  int i = 0;
   for (object_base *obj : objs)
     {
       {
         get_text_for_gui_policy *pl = obj->get_policy<get_text_for_gui_policy> ();
         if (pl)
           local_result->add_element (make_unique<gui_textline_t> (
-              engine.get_renderer (), 10, 10, gui_horizontal_alignment_t::LEFT,
-              gui_vertical_alignment_t::DOWN, pl->get_text (), glm::vec3 (.7f, .15f, .15f), 48));
+              engine.get_renderer (), 10, ++i * 24, gui_horizontal_alignment_t::LEFT,
+              gui_vertical_alignment_t::DOWN, pl->get_text (), glm::vec3 (.7f, .15f, .15f), 24));
       }
       // other types of gui elements here...
     }
