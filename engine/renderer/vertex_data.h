c4c9cd7ec8c145102224f444078bb0923886cd24
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-02-10 01:31:36 +0300

    NEW: renderer: add vertex_data struct and helper struct

---
+++ engine/renderer/vertex_data.h
@@ -0,0 +1,30 @@
+#ifndef VERTEX_DATA_H
+#define VERTEX_DATA_H
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
+struct vertex_data_t
+{
+  glm::vec3 m_vertex;
+  glm::vec3 m_normal;
+  glm::vec2 m_uv;
+  vertex_data_t (glm::vec3 vertex, glm::vec3 normal, glm::vec2 uv):
+    m_vertex (vertex), m_normal (normal), m_uv (uv)
+  {}
+  ~vertex_data_t () {}
+};
+
+struct vertex_data_view_t
+{
+  const glm::vec3 &m_vertex;
+  const glm::vec3 &m_normal;
+  const glm::vec2 &m_uv;
+  vertex_data_view_t (const glm::vec3 &vertex, const glm::vec3 &normal, const glm::vec2 &uv):
+    m_vertex (vertex), m_normal (normal), m_uv (uv)
+  {}
+  vertex_data_view_t (const vertex_data_t &vertex_data):
+    m_vertex (vertex_data.m_vertex), m_normal (vertex_data.m_normal), m_uv (vertex_data.m_uv)
+  {}
+};
+
+#endif // VERTEX_DATA_H
