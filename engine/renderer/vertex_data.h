3a4001e9218caafa4fed8d08f1546ffc352aef34
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-02-10 03:35:21 +0300

    REF: renderer: move vertex_data_t constructor to .cpp

--- engine/renderer/vertex_data.h
+++ engine/renderer/vertex_data.h
@@ -3,14 +3,16 @@
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 
+struct vertex_data_view_t;
+
+
 struct vertex_data_t
 {
   glm::vec3 m_vertex;
   glm::vec3 m_normal;
   glm::vec2 m_uv;
-  vertex_data_t (glm::vec3 vertex, glm::vec3 normal, glm::vec2 uv):
-    m_vertex (vertex), m_normal (normal), m_uv (uv)
-  {}
+  vertex_data_t (glm::vec3 &&vertex, glm::vec3 &&normal, glm::vec2 &&uv);
+  vertex_data_t (const vertex_data_view_t &vertex_view);
   ~vertex_data_t () {}
 };
 
