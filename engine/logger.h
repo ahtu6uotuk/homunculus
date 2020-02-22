16948b4cfc9e972fd087222218e75df5755e3aa5
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-02-22 16:23:33 +0300

    ENH: engine: add print_plain func to logger_t

--- engine/logger.h
+++ engine/logger.h
@@ -44,17 +44,26 @@ public:
   logger_t () = delete;
   explicit logger_t (const char *fname_log);
   template<typename ...Types>
-  void print (const log_section_t section, const log_priority_t priority, const Types & ... data)
+  void print_plain (const Types & ... data)
   {
     lock_guard lock (m_mutex);
     m_buffer.clear ();
     m_buffer.str ("");
-    m_buffer << enum_to_string (section) << ": " << enum_to_string (priority) << ":";
     print_internal (data...);
     m_buffer << std::endl;
     m_fstream << m_buffer.rdbuf ();
     m_fstream.flush ();
   }
+  template<typename ...Types>
+  void print (const log_section_t section, const log_priority_t priority, const Types & ... data)
+  {
+    std::string prefix = enum_to_string (section);
+    prefix.append (": ");
+    prefix.append (enum_to_string (priority));
+    prefix.append (": ");
+    return print_plain (prefix, data...);
+  }
+
   bool is_ok () const;
   ~logger_t ();
 };
