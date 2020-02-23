ad3bb0b28a44ca6b556c5c532c51204fa3e56fe1
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-02-23 17:44:30 +0300

    FIX: renderer: change vertex_data structs to use normally with std::map

--- engine/renderer/vertex_data.cpp
+++ engine/renderer/vertex_data.cpp
@@ -1,11 +1,14 @@
 #include "vertex_data.h"
+#include <cstring>
 
-vertex_data_t::vertex_data_t(glm::vec3 &&vertex, glm::vec3 &&normal, glm::vec2 &&uv):
-  m_vertex (vertex), m_normal (normal), m_uv (uv)
-{}
 
 vertex_data_t::vertex_data_t (const vertex_data_view_t &vertex_view):
-  m_vertex (vertex_view.m_vertex),
-  m_normal (vertex_view.m_normal),
-  m_uv (vertex_view.m_uv)
+  m_vertex (*vertex_view.m_vertex),
+  m_normal (*vertex_view.m_normal),
+  m_uv (*vertex_view.m_uv)
 {}
+
+bool vertex_data_view_t::operator< (const vertex_data_view_t &b) const
+{
+  return std::memcmp (this, &b, sizeof (vertex_data_view_t)) > 0;
+};
--- engine/renderer/vertex_data.h
+++ engine/renderer/vertex_data.h
@@ -11,26 +11,22 @@ struct vertex_data_t
   glm::vec3 m_vertex;
   glm::vec3 m_normal;
   glm::vec2 m_uv;
-  vertex_data_t (glm::vec3 &&vertex, glm::vec3 &&normal, glm::vec2 &&uv);
   vertex_data_t (const vertex_data_view_t &vertex_view);
   ~vertex_data_t () {}
 };
 
 struct vertex_data_view_t
 {
-  const glm::vec3 &m_vertex;
-  const glm::vec3 &m_normal;
-  const glm::vec2 &m_uv;
+  const glm::vec3 *m_vertex;
+  const glm::vec3 *m_normal;
+  const glm::vec2 *m_uv;
   vertex_data_view_t (const glm::vec3 &vertex, const glm::vec3 &normal, const glm::vec2 &uv):
-    m_vertex (vertex), m_normal (normal), m_uv (uv)
+    m_vertex (&vertex), m_normal (&normal), m_uv (&uv)
   {}
   vertex_data_view_t (const vertex_data_t &vertex_data):
-    m_vertex (vertex_data.m_vertex), m_normal (vertex_data.m_normal), m_uv (vertex_data.m_uv)
+    m_vertex (&vertex_data.m_vertex), m_normal (&vertex_data.m_normal), m_uv (&vertex_data.m_uv)
   {}
-  bool operator< (const vertex_data_view_t &b) const
-  {
-    return m_vertex == b.m_vertex && m_normal == b.m_normal && m_uv == b.m_uv;
-  };
+  bool operator< (const vertex_data_view_t &b) const;
 };
 
 #endif // VERTEX_DATA_H
