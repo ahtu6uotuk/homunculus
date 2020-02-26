ce5f257b731dabb41caa2c1e5a87cb69759b668d
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-02-27 01:34:46 +0300

    REF: renderer: remove test shader loading block

--- engine/renderer/renderer.cpp
+++ engine/renderer/renderer.cpp
@@ -5,57 +5,18 @@
 #include "engine/renderer/shader.h"
 
 
-// TODO: remove this function
-string test_get_shader_src (const char *fname);
-string test_get_shader_src (const char *fname)
-{
-  ifstream t (fname);
-  std::string str;
-
-  t.seekg(0, std::ios::end);
-  str.reserve(t.tellg());
-  t.seekg(0, std::ios::beg);
-
-  str.assign((std::istreambuf_iterator<char>(t)),
-              std::istreambuf_iterator<char>());
-  return str;
-}
-
 renderer_t::renderer_t (engine_t &engine):
   m_engine (engine),
   m_window (engine.get_sfml_window ()),
   m_gui (engine.get_gui_system ()),
   m_font ("FreeSerif.ttf", 48),
-  m_text_shader (nullptr)
 {}
 
-#include <glm/common.hpp>
-#include <glm/gtc/type_ptr.hpp>
 err_t renderer_t::init ()
 {
   glewExperimental = GL_TRUE;
   if (glewInit () != GLEW_OK)
     return err_t ("failed to init OpenGL!");
-
-  // TODO: remove this block
-  // BLOCK BEGIN
-  // ===>>>
-  auto vs = subshader_t<GL_VERTEX_SHADER> (test_get_shader_src ("gamedata/shaders/text.vert").c_str ());
-  RETURN_IF_FAIL (vs.check_compilation_status ());
-
-  auto fs = subshader_t<GL_FRAGMENT_SHADER> (test_get_shader_src ("gamedata/shaders/text.frag").c_str ());
-  RETURN_IF_FAIL (fs.check_compilation_status ());
-  m_text_shader = make_unique<shader_t> (
-        vs.get_id (),
-        fs.get_id ()
-        );
-  glm::mat4 projection = glm::ortho (0.f, 800.f, 0.f, 600.f);
-  RETURN_IF_FAIL (m_text_shader->check_link_status ());
-
-  m_text_shader->use ();
-  glUniformMatrix4fv(glGetUniformLocation (m_text_shader->get_program_id (), "projection"), 1, GL_FALSE, glm::value_ptr (projection));
-  // <<<===
-  // BLOCK END
 
   glEnable (GL_DEPTH_TEST);
   glEnable (GL_CULL_FACE);
@@ -68,8 +29,6 @@ err_t renderer_t::init ()
 
   glViewport (0., 0., 800, 600);
 
-  init_gui_gl_objects ();
-
   return ERR_OK;
 }
 
@@ -77,15 +36,11 @@ void renderer_t::render ()
 {
   glClearColor (0., 0., 0., 0.);
   glClear (GL_COLOR_BUFFER_BIT);
+  //... Draw something here
+  glPushMatrix ();
   gluOrtho2D (0., 800, 0., 600.);
   m_gui.draw ();
-}
-
-void renderer_t::render_text (const string &text, glm::vec2 &pos, glm::vec3 &color, const unsigned int font_size)
-{
-  m_text_shader->use ();
-  glUniform3f (glGetUniformLocation (m_text_shader->get_program_id (), "textColor"), color.r, color.g, color.b);
-  m_font.render_text (text, pos.x, pos.y, m_font.get_scale (font_size));
+  glPopMatrix ();
 }
 
 renderer_t::~renderer_t ()
--- engine/renderer/renderer.h
+++ engine/renderer/renderer.h
@@ -2,7 +2,6 @@
 #define RENDERER_H
 #include "common/err_t.h"
 #include "engine/renderer/font.h"
-#include "engine/renderer/shader.h"
 
 class engine_t;
 class gui_system_t;
@@ -19,14 +18,11 @@ class renderer_t
   sf::RenderWindow &m_window;
   gui_system_t &m_gui;
   font_t m_font;
-  unique_ptr<shader_t> m_text_shader;
 public:
   renderer_t (engine_t &engine);
   err_t init ();
   void render ();
-  void render_text (const string &text, glm::vec2 &pos, glm::vec3 &color, const unsigned int font_size);
   font_t &get_font () {return m_font;}
-  shader_t *get_text_shader () {return m_text_shader.get ();}
   gui_system_t &get_gui () {return m_gui;}
   ~renderer_t ();
 };
