c99cc9d9a3e99147f7397e2d454fe20784ac13a3
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-04-04 22:24:11 +0300

    Revert "REF: main: print log error intead of assert"
    This reverts commit bb12eb8055f40c26fcd2d9d49896cfe5e2e10462.

--- main.cpp
+++ main.cpp
@@ -19,9 +19,6 @@ int main (int argc, char *argv[])
     }
 
   control_flow flow (argc, argv);
-  err_t err = flow.run ();
-  if (err)
-    logger_t::instance ().print (log_section_t::ENGINE, log_priority_t::LOG_ERROR, "execution aborted");
-
+  assert_error (flow.run (), "Error occured in while running: ");
   return 0;
 }
