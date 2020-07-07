08f4a1e1058938289a9f60da96c522a875e5eeb4
Alexey Kolyaskin <kolyaskin.a.d@gmail.com>
2020-07-07 20:15:43 +0300

    ENH: world: add model-position policies to friend class

--- world/classes/friend.h
+++ world/classes/friend.h
@@ -8,6 +8,11 @@
 #include "world/policies/plot_tags_policy.h"
 #include "world/policies/talk_policy.h"
 
+#include "world/policies/shader_policy.h"
+#include "world/policies/mesh_policy.h"
+#include "world/policies/texture_policy.h"
+#include "world/policies/position_policy.h"
+
 enum class mood { happy, sad, melancholic, COUNT };
 const char *enum_to_string (mood m);
 enum class bloodtype { o, a, b, ab, COUNT };
@@ -24,7 +29,13 @@ class friend_t
         simple_name_policy,
         simple_get_text_for_gui_policy,
         simple_plot_tags_policy<named_enum<LITERAL ("mood"), mood>, named_enum<LITERAL ("blood_type"), bloodtype>>,
-        simple_talk_policy<LITERAL ("assets/some_dialog.xml")>>
+        simple_talk_policy<LITERAL ("assets/some_dialog.xml")>,
+        // perhaps these three should be merged into one policy, its not even hard
+        simple_shader_policy<LITERAL("shaders/test.shader")>,
+        simple_mesh_policy<LITERAL("models/cube.obj")>,
+        simple_texture_policy<LITERAL("textures/cube_friend.tga")>,
+        position_policy_t
+        >
 {
 public:
   static std::string objtype_name () { return "friend"; }
